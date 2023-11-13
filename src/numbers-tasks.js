8e6d87ebc9abf4cbb2afe7159aa99d252aff4150
xmakcxx <xmakcxx@mail.ru>
2023-11-13 22:22:21 +0300

    fix: new task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -450,8 +450,8 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return parseFloat(str);
 }
 
 /**
